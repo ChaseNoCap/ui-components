bd8d427702d95d3aa614ab5914ceffed7883d5f0
ChaseNoCap <joshindaphils+chasenocap@gmail.com>
2025-05-30 06:40:09 -0400

    fix: path fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/services': path.resolve(__dirname, './src/services'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/contexts': path.resolve(__dirname, './src/contexts'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
     },
   },
   server: {
